b465bc93fb7561645ef0e7793453a90ad7444279
Zoo Sky <zoosky@gmail.com>
2023-09-25 15:36:17 +0200

    exercise fixes

--- exercise/docs/src/lib.rs
+++ exercise/docs/src/lib.rs
@@ -1,12 +1,12 @@
 // 1. Thank you for volunteering to document our pumpkin library! Let's start by grabbing the first
 // paragraph from https://en.wikipedia.org/wiki/Pumpkin and pasting it as our module-level
 // documentation. Hint: Use inner-documentation comments.
-//! 
-//! A pumpkin is a vernacular term for mature winter squash of species and varieties in the genus Cucurbita 
-//! that has culinary and cultural significance[1][2] but no agreed upon botanical or scientific meaning. 
-//! The term pumpkin is sometimes used interchangeably with "squash" or "winter squash", and is commonly used for 
+//!
+//! A pumpkin is a vernacular term for mature winter squash of species and varieties in the genus Cucurbita
+//! that has culinary and cultural significance[1][2] but no agreed upon botanical or scientific meaning.
+//! The term pumpkin is sometimes used interchangeably with "squash" or "winter squash", and is commonly used for
 //! cultivars of Cucurbita argyrosperma, Cucurbita ficifolia, Cucurbita maxima, Cucurbita moschata, and Cucurbita pepo.
-//! 
+//!
 
 // Once you've got the documentation here, run `cargo doc --no-deps --open` and take a look!
 
@@ -14,7 +14,7 @@
 // The markdown format is ![some alt text](https://url-to-the-image.png)
 // Here's the image to link to: https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg
 
-//! ![some alt text](https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg)
+//! ![Pumkin Image](https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg)
 
 // 3. Document the Pumpkin struct.
 // - The description on the index page should be "Big orange thing"
@@ -23,26 +23,30 @@
 // - Document the "roundness" field, explaining that it is a percentage
 // - Document the "orangeness" field, explaining that it is a number from 8 to 27
 
-
-/// "Big orange thing 
+/// Big orange thing
+///
+///  # Recipies
+///
+/// Recipies will mostly assuredly be comming very, very soon!
 pub struct Pumpkin {
-    /// roundness in percentage
+    /// `roundness` is a percentage
     pub roundness: f32,
-    // orangeness in number from 8 to 27
-    pub orangeness: i32,
+    /// `orangeness` is a number from 8 to 27
+    pub orangeness: i32, 
 }
 
 // 4. Document the "smash" method. Explain that if you smash the pumpkin, it will be gone. Then it
 // can't be used for pie. :'-(
 
 impl Pumpkin {
-    /// destroyes a pumkin.
+    ///  If you smash the pumpkin, it will be gone.
+    /// Then it can't be used for pie. 😭
     pub fn smash(self) {}
 }
 
 // 5. Document that BURNT_ORANGE is for the "orangeness" field in the Pumpkin struct.
 // - Link to the Pumpkin struct in your description
-/// Used for [Pumpkin::orangeness] 
+/// `BURNT_ORANGE` is used for the field [Pumpkin::orangeness]
 pub const BURNT_ORANGE: i32 = 13;
 
 // Challenge: Find the option to pass to `cargo doc` so that documentation for this private item
